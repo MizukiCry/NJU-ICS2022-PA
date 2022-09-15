85e38150318e80d99b70ebb725f50ece4bfcda06
tracer-ics2022 <tracer@njuics.org>
2022-09-15 22:45:58 +0800

    >  compile NEMU THU_STUID MizukiCry Linux mizlinux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:45:58 up  3:18,  3 users,  load average: 0.37, 0.21, 0.33

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,7 +20,6 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-
 word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
@@ -32,6 +31,7 @@ int main(int argc, char *argv[]) {
   bool x_state;
   char _expr[65536];
   while (~fscanf(f, "%u%[^\n]", &x, _expr)) {
+    printf("Start\n");
     uint32_t res = expr(_expr, &x_state);
     assert(x_state);
     assert(x == res);
