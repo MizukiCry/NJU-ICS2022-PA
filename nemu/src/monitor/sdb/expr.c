1ce79caef10e6105aad0e0a9c9a315285d77cf57
tracer-ics2022 <tracer@njuics.org>
2022-09-15 22:47:17 +0800

    >  compile NEMU THU_STUID MizukiCry Linux mizlinux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:47:17 up  3:19,  3 users,  load average: 0.76, 0.43, 0.40

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -85,6 +85,7 @@ static Token tokens[1024] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
+  printf("Start make token\n");
   int position = 0;
   int i;
   regmatch_t pmatch;
