c77ce1ed4eb2845f873c579cd9716456e6770e2b
tracer-ics2022 <tracer@njuics.org>
2022-09-16 10:40:35 +0800

    >  compile NEMU THU_STUID MizukiCry Linux mizlinux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:35 up  5:14,  3 users,  load average: 0.07, 0.26, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -106,6 +106,8 @@ static bool make_token(char *e) {
       return false;
     }
 
+    printf("--? [%d] [(%d)%c]", position, (int)e[position], e[position]);
+
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
