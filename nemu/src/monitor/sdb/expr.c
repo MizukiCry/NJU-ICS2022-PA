2d56356a11bb05852fa72f60dcab656303dd186e
tracer-ics2022 <tracer@njuics.org>
2022-09-21 14:14:57 +0800

    >  compile NEMU THU_STUID MizukiCry Linux mizlinux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:57 up 1 day, 20:42,  3 users,  load average: 0.70, 0.66, 0.57

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -321,10 +321,10 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  printf("Made token [%d]\n", nr_token);
+  /*printf("Made token [%d]\n", nr_token);
   for (int i = 0; i < nr_token; ++i) {
     printf("-- %d\n", tokens[i].type);
-  }
+  }*/
   *success = true;
   return eval(0, nr_token - 1, success);
 }
