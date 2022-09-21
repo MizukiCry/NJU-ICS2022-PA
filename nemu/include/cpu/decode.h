27c2e69c9477c0b6c2c12b7d1ad7d1a341393cb7
tracer-ics2022 <tracer@njuics.org>
2022-09-21 12:11:35 +0800

    >  compile NEMU THU_STUID MizukiCry Linux mizlinux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:11:35 up 1 day, 19:50,  3 users,  load average: 0.56, 0.52, 0.30

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -90,9 +90,7 @@ finish:
 #define INSTPAT(pattern, ...) do { \
   uint64_t key, mask, shift; \
   pattern_decode(pattern, STRLEN(pattern), &key, &mask, &shift); \
-  printf("INSTPAT %lu %lu %lu\n", key, mask, shift); \
   if (((INSTPAT_INST(s) >> shift) & mask) == key) { \
-    printf("INSTPAT MATCHED\n"); \
     INSTPAT_MATCH(s, ##__VA_ARGS__); \
     goto *(__instpat_end); \
   } \
